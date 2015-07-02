36fd5cebf3f27be437b51c8a2563f3fc672e182c
Ayrton Stout <ayrtonstout@u.boisestate.edu>
2015-07-01 23:58:53 -0600

    Added collision and pac pellet eating

--- src-common/pacman/core.clj
+++ src-common/pacman/core.clj
@@ -5,10 +5,46 @@
             [play-clj.ui :refer :all]))
 
 (def background (atom nil))
+(def pellet (atom nil))
+(def dead (atom false))
 (def key-pressed (atom {:up    false
                         :down  false
                         :right false
                         :left  false}))
+
+(defn create-dot
+  [x y]
+  (assoc @pellet :type :pellet-small, :x x, :y y, :width 3, :height 3))
+
+
+(defn create-all-dots
+  []
+  (-> []
+      (conj (map create-dot (range 45 221 16) (repeat 475)))
+      (conj (map create-dot (range 45 429 16) (repeat 410)))
+      (conj (map create-dot (range 45 109 16) (repeat 362)))
+      (conj (map create-dot (range 365 425 16) (repeat 362)))
+      (conj (map create-dot (range 253 429 16) (repeat 475)))
+      (conj (map create-dot (repeat 29) (range 362 481 16)))
+      (conj (map create-dot (repeat 429) (range 362 481 16)))
+      (conj (map create-dot (repeat 109) (range 74 469 16)))
+      (conj (map create-dot (repeat 349) (range 74 469 16)))
+      (conj (map create-dot (range 45 115 16) (repeat 170)))
+      (conj (map create-dot (range 349 425 16) (repeat 170)))
+      (conj (map create-dot (repeat 29) (range 122 172 16)))
+      (conj (map create-dot (repeat 429) (range 122 172 16)))
+      (conj (create-dot 45 122))
+      (conj (create-dot 413 122))
+      (conj (map create-dot (repeat 61) (range 90 130 16)))
+      (conj (map create-dot (repeat 397) (range 90 130 16)))
+      (conj (map create-dot (range 45 109 16) (repeat 74)))
+      (conj (map create-dot (range 365 425 16) (repeat 74)))
+      (conj (map create-dot (repeat 29) (range 42 90 16)))
+      (conj (map create-dot (repeat 429) (range 42 90 16)))
+      (conj (map create-dot (range 29 445 16) (repeat 26)))
+      (conj (map create-dot (repeat 205) (range 426 459 16)))
+      (conj (map create-dot (repeat 253) (range 426 459 16)))
+      ))
 
 (defn within?
   [num low high]
@@ -32,15 +68,70 @@
     :none false))
 
 (defn spawn-clones
-  [entities1]
-  (loop [entities entities1 i (dec (count entities1))]
-    (if (< i 0)
+  [entities]
+  (loop [entities entities i (dec (count entities))]
+    (if (< i 2)
       entities
       (let [curr (nth entities i)]
         (cond
-          (= (:x curr) 0) (recur (conj entities (assoc (texture (:object curr)) :id (:id curr), :type (:type curr), :x (- (game :width) 1), :y (:y curr), :width 26, :height 26, :direction :left, :cooldown 10)) (dec i))
-          (= (:x curr) (game :width)) (recur entities (dec i))
+          (and (= (:x curr) 0) (= (:direction curr) :left)) (recur (conj entities (assoc curr :x (dec (game :width)))) (dec i))
+          (and (= (:x curr) (- (game :width) 26)) (= (:direction curr) :right)) (recur (conj entities (assoc curr :x -25)) (dec i))
           :else (recur entities (dec i)))))))
+
+(defn clear-clones
+  [entities]
+  (loop [return [] i 0]
+    (if (= i (count entities))
+      return
+      (let [curr (nth entities i)]
+        (if (and (>= (:x curr) -26) (<= (:x curr) (dec (game :width)))) ;;TODO make 26 adaptive (:width curr) in this and spawn functions
+          (recur (conj return curr) (inc i))
+          (recur return (inc i)))))))
+
+(defn collision?
+  [guy1 guy2]
+  (let [x1 (:x guy1) x2 (:x guy2)
+        y1 (:y guy1) y2 (:y guy2)
+        width1 (dec (:width guy1)) width2 (dec (:width guy2))
+        height1 (dec (:height guy1)) height2 (dec (:height guy2))]
+    (and
+      (< x1 (+ x2 width2))
+      (> (+ x1 width1) x2)
+      (< y1 (+ y2 height2))
+      (> (+ height1 y1) y2))))
+
+(defn collision-list
+  [entities]
+  (remove false? (for [i (range 2 (count entities))
+                       j (range 2 (count entities))]
+                   (if (= (:type (nth entities i)) :player)
+                     (case (:type (nth entities j))
+                       :ghost (collision? (nth entities i) (nth entities j))
+                       :pellet-small (if (collision? (nth entities i) (nth entities j))
+                                       j
+                                       false)
+                       false)
+                     false))))
+
+(defn drop-nth
+  [coll pos]
+  (if (integer? pos)
+    (vec (concat (subvec coll 0 pos) (subvec coll (inc pos))))
+    coll))
+
+(defn increment-score!
+  [entities]
+  (do
+    (label! (second entities) :set-text (str (+ (:score (second entities)) 10)))
+    (assoc (second entities) :score (+ (:score (second entities))))))
+
+(defn manage-collisions!
+  [entities]
+  (let [list (collision-list entities)]
+    (do
+      (reset! dead (= true (first (filter true? list))))
+      (increment-score! (second entities))
+      (drop-nth entities (first (filter integer? list))))))
 
 (defn valid-turn
   [entity direction]
@@ -96,7 +187,6 @@
                   (assoc :angle 180)))
       pacman)))
 
-
 (defn move-AI
   [ghost]
   (if (<= (:cooldown ghost) 0)
@@ -135,17 +225,20 @@
              (add-timer! screen :animation 1 0.075)
              (update! screen :renderer (stage))
              (reset! background (pixmap "background.png"))
+             (reset! pellet (texture "pellet-small.png"))
 
              (let [background (assoc (texture "background.png") :type :ui, :x 1 :y 0)
+                   score-text (assoc (label "Score: 0" (color :white)) :type :ui, :x 35, :y 485, :score 0)
                    pacman (assoc (texture "pacman-sheet.png") :id :pacman, :type :player, :x 208, :y 206, :width 26, :height 26, :direction :right)
                    ;(texture! pacman :set-region-x 64)
                    ;(texture! pacman :set-region-width 32)
-                   blinky (assoc (texture "blinky.png") :id :blinky, :type :ghost, :x 75, :y 350, :width 26, :height 26, :direction :none, :cooldown 0)
-                   pinky (assoc (texture "pinky.png") :id :pinky, :type :ghost, :x 135, :y 350, :width 26, :height 26, :direction :none, :cooldown 0)
-                   inky (assoc (texture "inky.png") :id :inky, :type :ghost, :x 195, :y 350, :width 26, :height 26, :direction :none, :cooldown 0)
-                   clyde (assoc (texture "clyde.png") :id :clyde, :type :ghost, :x 255, :y 350, :width 26, :height 26, :direction :none, :cooldown 0)
-                   score-text (assoc (label "Score: 1337" (color :white)) :type :ui, :x 35, :y 484)]
-               [background pacman blinky pinky inky clyde score-text]))
+                   blinky (assoc (texture "Adam-glow.png") :id :blinky, :type :ghost, :x 75, :y 350, :width 26, :height 26, :direction :none, :cooldown 0)
+                   pinky (assoc (texture "Zach-small.png") :id :pinky, :type :ghost, :x 135, :y 350, :width 26, :height 26, :direction :none, :cooldown 0)
+                   inky (assoc (texture "Kevin-small.png") :id :inky, :type :ghost, :x 195, :y 350, :width 26, :height 26, :direction :none, :cooldown 0)
+                   clyde (assoc (texture "Jacob-small.png") :id :clyde, :type :ghost, :x 255, :y 350, :width 26, :height 26, :direction :none, :cooldown 0)
+                   dots (create-all-dots)
+                   ]
+               (flatten [background score-text dots pacman blinky pinky inky clyde])))
 
            :on-render
            (fn [screen entities]
@@ -194,27 +287,32 @@
            (fn [screen entities]
              (case (:id screen)
                :systick
-               (let [new-entities (spawn-clones entities)]
-                 (reduce (fn [coll e]
-                           (conj coll
-                                 (case (:type e)
-                                   :ghost (move-AI e)
-                                   :player (move-entity (change-direction e))
-                                   e)))
-                         []
-                         new-entities))
+               ;(if (dead? entities)
+                 ;(println "dead")
+                 (let [new-entities (clear-clones (spawn-clones entities))]
+                   (manage-collisions! (reduce (fn [coll e]
+                                               (conj coll
+                                                     (case (:type e)
+                                                       :ghost (move-AI e)
+                                                       :player (move-entity (change-direction e))
+                                                       e)))
+                                             []
+                                             new-entities)))
+               ;)
                :animation
                (reduce (fn [coll e]
                          (conj coll
-                              (case (:type e)
-                                 :ghost (identity e)
-                                 :player (if (not= (:direction e) :none)
+                              (case (:id e)
+                                 :pacman (if (not= (:direction e) :none)
                                            (do
                                              (texture! e :set-region-x (mod (+ (texture! e :get-region-x) 32) 96))
                                              (texture! e :set-region-width 32)
                                              e)
                                            e)
-                                 :ui (identity e)
+                                 :blinky (do
+                                           (texture! e :set-region-x (mod (+ (texture! e :get-region-x) 26) 416))
+                                           (texture! e :set-region-width 26)
+                                           e)
                                  e)))
                        []
                        entities))))
